c1acf465d9064667317318eaa2b35d256e444cf1
shoyo <senku_snap@protonmail.com>
2023-05-16 02:17:40 +0530

    used utiltiy.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./globalStyles/reset.css";
 import "./globalStyles/variables.css";
 import "./globalStyles/utility.css";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 function App() {
   return (
     <div className="App">

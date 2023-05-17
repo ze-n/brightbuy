5494720f31246973f595cf581cdaba07af12be68
shoyo <senku_snap@protonmail.com>
2023-05-17 07:18:53 +0530

    finished Blob

--- src/components/Sections/Hero/Hero.jsx
+++ src/components/Sections/Hero/Hero.jsx
@@ -10,18 +10,21 @@ const Hero = () => {
     <Wrapper>
       <div className="container flex-columns">
         <div className="text-column flex-column">
-          <div className="rating">
-            <SpecialRating />
+          <div className="text-container">
+            <div className="rating">
+              <SpecialRating />
+            </div>
+            <h1 className="title">
+              Discover the Best Selection of Products for Your Lifestyle
+            </h1>
+            <h2 className="sub-title">
+              We offers a broad range of products to meet your needs.
+            </h2>
+            <div className="cta-btn">
+              <PlainBtn text="Shop Best Sellers" />
+            </div>
           </div>
-          <h1 className="title">
-            Discover the Best Selection of Products for Your Lifestyle
-          </h1>
-          <h2 className="sub-title">
-            We offers a broad range of products to meet your needs.
-          </h2>
-          <div className="cta-btn">
-            <PlainBtn text="Shop Best Sellers" />
-          </div>
+          <img src={blob} alt="" className="blob" />
         </div>
         <div className="image-column flex-column">
           <img src={heroImg} alt="" className="hero-img" />
@@ -53,10 +56,14 @@ const Wrapper = styled.section`
   /*  */
   /* setting up basic height width */
   min-width: 100vw;
+  /* this is given because only one word is left on second line otherwise which looks bad */
   .sub-title {
     max-width: 25ch;
   }
-  /* centering text vertically */
+
+  /* centering text-container vertically */
+  /* this is done because text-column has more height than required because of flex:1 on flex-columns */
+  /* not so sure about why height is more */
   .text-column {
     display: flex;
     justify-content: center;
@@ -87,6 +94,8 @@ const Wrapper = styled.section`
     transform: scale(1.1) translateX(5%);
   }
   /* aliging decoration images */
+
+  /* blobs stuff */
   /* setting up image-column to be relative */
   .image-column {
     position: relative;
@@ -103,27 +112,49 @@ const Wrapper = styled.section`
     z-index: 1;
   }
 
+  /* blob stuff */
+  /* setting up text-column to be relative */
+  .text-column {
+    position: relative;
+  }
+  /* positioning blob */
+  .blob {
+    position: absolute;
+    top: 10%;
+    left: -50%;
+  }
+  /* setting up text-container to appear at top */
+  .text-container {
+    position: relative;
+    z-index: 1;
+  }
   @media screen and (max-width: 750px) {
     /* setting up padding */
     --pinline: 2rem;
     /* fixing size of image column in mobile devices */
+    /* because it was 45% on big screens */
     .image-column {
       max-width: 100%;
     }
     /* centering everything inside text column horizontally */
-    .text-column {
+    .text-container {
+      display: flex;
+      flex-direction: column;
       align-items: center;
       text-align: center;
+    }
+    /* aligning image-column to be centered horizontally */
+    /* this works because image-column is child of flex-columns */
+    .image-column {
+      align-self: center;
     }
     /* fixing position of button */
     .cta-btn {
       transform: scale(1.1);
     }
-    /* centering flex columns horizontally */
-    .flex-columns {
-      align-items: center;
-    }
+
     /* aligning decoration images */
+    /* blobs stuff */
     .blobs {
       top: -5%;
     }

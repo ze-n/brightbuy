97c2a88d413369c25806a24ab517dfb55e3f6b79
shoyo <senku_snap@protonmail.com>
2023-05-18 03:25:40 +0530

    used BestSellingProducts component section inside Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "../../components/Sections/Hero/Hero";
 import heroImage from "../../assets/hero-img.png";
+import BestSellingProducts from "./BestSellingProducts";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         btnText="Shop Best Sellers"
         heroImg={heroImage}
       />
+      <BestSellingProducts />
     </div>
   );
 };

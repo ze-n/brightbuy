5c1e78db2d25b88c892a4a08f9940753bea0b179
shoyo <senku_snap@protonmail.com>
2023-05-17 07:36:34 +0530

    Home component now calls Hero using props

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Hero from "../../components/Sections/Hero/Hero";
+import heroImage from "../../assets/hero-img.png";
 
 const Home = () => {
   return (
     <div>
-      <Hero />
+      <Hero
+        title="  Discover the Best Selection of Products for Your Lifestyle"
+        subTitle="We offers a broad range of products to meet your needs."
+        btnText="Shop Best Sellers"
+        heroImg={heroImage}
+      />
     </div>
   );
 };

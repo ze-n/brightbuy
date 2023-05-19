3cb1912940edc29d06f0c8faf7d7d8a96ac4d64e
shoyo <senku_snap@protonmail.com>
2023-05-19 17:04:17 +0530

    added horizontal scroll in app

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import NotificationBar from "./components/NotificationBar";
 import Home from "./pages/Home/Home";
+import HorizontalScroll from "./components/HorizontalScroll";
 function App() {
   return (
     <div className="App">
       <NotificationBar />
       <Header />
       <Home />
+      <HorizontalScroll />
       <Footer />
     </div>
   );

324f1e9a88d13e7778a2ecbb12aa2b80089b424b
shoyo <senku_snap@protonmail.com>
2023-05-20 16:05:21 +0530

    removed HorizontalScroll from App and added in Home

--- src/App.js
+++ src/App.js
@@ -5,16 +5,13 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import NotificationBar from "./components/NotificationBar";
 import Home from "./pages/Home/Home";
-import HorizontalScroll from "./components/HorizontalScroll";
-import ClothingLine from "./pages/Home/ClothingLine";
+
 function App() {
   return (
     <div className="App">
       <NotificationBar />
       <Header />
       <Home />
-      <HorizontalScroll />
-      <ClothingLine />
       <Footer />
     </div>
   );

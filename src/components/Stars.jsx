44f553f62bbb4832639bfbf43e871e222e8d8786
shoyo <senku_snap@protonmail.com>
2023-06-01 16:41:01 +0530

    created a Stars component

---
+++ src/components/Stars.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+import { AiOutlineStar } from "react-icons/ai";
+const Stars = ({ rating }) => {
+  const ratingStar = Array.from({ length: 5 }, (elem, index) => {
+    let number = index + 0.5;
+    return (
+      <span key={index}>
+        {rating >= index + 1 ? (
+          <FaStar className="icon" />
+        ) : rating >= number ? (
+          <FaStarHalfAlt className="icon" />
+        ) : (
+          <AiOutlineStar className="icon" />
+        )}
+      </span>
+    );
+  });
+
+  return <>{ratingStar}</>;
+};
+
+export default Stars;

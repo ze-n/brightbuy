55e6fd3eb1ab44a7d1c2e047866e8bfbdf2a3160
shoyo <senku_snap@protonmail.com>
2023-06-14 19:30:55 +0530

    added more methods in UserAuthContext and also cleaned up code'

--- src/context/UserAuthContext.js
+++ src/context/UserAuthContext.js
@@ -1,4 +1,4 @@
-import { useContext, createContext, useEffect, useState } from "react";
+import { useContext, createContext, useState } from "react";
 
 import {
   // AuthErrorCodes,
@@ -7,12 +7,22 @@ import {
   signInWithEmailAndPassword,
   signOut,
   // getAuth,
-  // sendEmailVerification,
+  sendEmailVerification,
   updateProfile,
+  updateEmail,
+  updatePassword,
+  deleteUser,
 } from "firebase/auth";
 import { auth, fs, storage } from "../Config/Config";
-import { addDoc, collection, doc, setDoc } from "firebase/firestore";
-import { ref, byteUpload, uploadBytes, getDownloadURL } from "firebase/storage";
+import {
+  // addDoc,
+  // collection,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 // created userContext
 const userContext = createContext();
@@ -28,12 +38,14 @@ const UserAuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState();
   // this fuction listens for changes in user authentication state, like when user logs in or logs out
   // and calls the callback function
+
   onAuthStateChanged(auth, (user) => {
     // console.log(user)
     // if user object exists i.e, if user is logged in
     if (user) {
       // set currentUser state to user object
       setCurrentUser(user);
+
       // set verifyEmail state to the emailVerified property of user object
       setVerifyEmail(user.emailVerified);
       console.log(verifyEmail + " in mail");
@@ -46,14 +58,53 @@ const UserAuthProvider = ({ children }) => {
   // profile information store
   // used to store profile information in firestore
 
-  const docRef = null;
-  const ProfileInformation = (profile) => {
-    console.log("storing");
-    docRef = addDoc(collection(fs, "profile"), profile);
+  // const docRef = null;
+
+  // this method is used to store data of new users inside SignUp method
+  async function storeUserData(userId, userData) {
+    // referring to users collection by using user uid
+    const userRef = doc(fs, "users", userId);
+    // creating a document with the help of userRef reference and data userData
+    await setDoc(userRef, userData);
+    console.log("User data updated successfully!");
+  }
+
+  // sign up User
+  const SignUp = async (userData) => {
+    // retriving data from userData object
+    const { Email, Password, Fullname } = userData;
+    // console.log(Email, Password, Fullname);
+    // creatin account
+    createUserWithEmailAndPassword(auth, Email, Password)
+      .then((res) => {
+        console.log("account made");
+        // adding name to auth displayname
+        updateProfile(res.user, {
+          displayName: Fullname,
+        });
+        // storing user data in firestore
+        storeUserData(res.user.uid, userData);
+
+        alert("account created, You are logged in automatically");
+        // sending email verification
+        sendEmailVerification(res.user)
+          .then(() => {
+            console.log("email verification sent");
+          })
+          .catch((error) => {
+            console.log(
+              "error occured while sending email verification ",
+              error
+            );
+          });
+      })
+      .catch((error) => {
+        console.log("error occured during sign up ", error);
+      });
   };
 
   // Login Functinallity
-
+  // This method is used to login
   const UserLogin = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
@@ -63,57 +114,134 @@ const UserAuthProvider = ({ children }) => {
     return signOut(auth);
   };
 
-  // sign User
-  const SignUp = async (email, password, FullName) => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((res) => {
-        updateProfile(res.user, {
-          displayName: FullName,
-        });
-      })
-      .catch((error) => {
-        console.log("error occured during sign up ", error);
-      });
-  };
-  // storage
-
   // upload
   const UploadAvatar = async (file, currentUser, setLoading) => {
     setLoading(true);
+    // storing image in databse
+    // creating reference of a file in database
     const fileRef = ref(storage, currentUser.uid + ".png");
+    // uploading file to that reference
     const snapshot = await uploadBytes(fileRef, file);
+    // storig source of file stored so that we can use it later
     const photoURL = await getDownloadURL(fileRef);
+    // giving this source to photoURL property of currentUser which is commonly used to refer to profile picture
     await updateProfile(currentUser, {
       photoURL,
     });
     setLoading(false);
     // console.log("photo urL ", currentUser.photoURL);
     // console.log("snapshot ", snapshot);
-    alert("uploaded file");
+    alert("Profile picture changed, refresh to see changes");
   };
+
+  // update name
   const UpdateName = async (currentUser, userName, setLoading) => {
     setLoading(true);
+    // referring to document inside users collection by using user uid
+    const docRef = doc(fs, "users", currentUser.uid);
+
+    // updating display name with new name
     updateProfile(currentUser, {
       displayName: userName,
     })
       .then(() => {
-        alert("user name changed");
+        // updating document with new name
+        updateDoc(docRef, {
+          Fullname: userName,
+        })
+          .then(() => {
+            alert("user name changed, refresh to see changes");
+          })
+          .catch((error) => {
+            console.log("error storing name in records ", error);
+          });
       })
       .catch((error) => {
         console.log("error updating name ", error);
       });
     setLoading(false);
   };
+
+  // update email
   const UpdateEmail = async (currentUser, userEmail, setLoading) => {
     setLoading(true);
-    updateProfile(currentUser, {
-      email: userEmail,
-    })
+    // creating a refrence to users collection using user uid
+    const docRef = doc(fs, "users", currentUser.uid);
+    // updating email
+    updateEmail(currentUser, userEmail)
       .then(() => {
-        alert("user email changed");
+        // storing new email in firestore
+        updateDoc(docRef, {
+          Email: userEmail,
+        })
+          .then(() => {
+            console.log("email registered in database ");
+          })
+          .catch((error) => {
+            console.log(
+              "some error occured registering email in database ",
+              error.message
+            );
+          });
+        alert("email changed ");
       })
       .catch((error) => {
-        console.log("something went wrong while changing email ", error);
+        console.log("email update error : ", error.message);
+        if (error.code == "auth/requires-recent-login") {
+          alert("please login and logout to perform this action ");
+        } else if (error.code == "auth/invalid-email") {
+          alert("Please enter valid email address");
+        } else {
+          alert("some error occured during changing email");
+        }
+      });
+
+    setLoading(false);
+  };
+  // update password
+  const UpdatePassword = async (currentUser, userPassword, setLoading) => {
+    setLoading(true);
+    const docRef = doc(fs, "users", currentUser.uid);
+    // updating password
+    updatePassword(currentUser, userPassword)
+      .then(() => {
+        // updating new password in firestore
+        updateDoc(docRef, {
+          Password: userPassword,
+        })
+          .then(() => {
+            console.log("password registered in database ");
+          })
+          .catch((error) => {
+            console.log(
+              "some error occured registering password in database ",
+              error.message
+            );
+          });
+        alert("password updated");
+      })
+      .catch((error) => {
+        if (error.code == "auth/requires-recent-login") {
+          alert("Please logout and login to perform this action");
+        } else {
+          alert("Error occured while updating password ");
+        }
+        console.log(error.message);
+      });
+    setLoading(false);
+  };
+
+  // delete user
+  const DeleteUser = async (currentUser, setLoading) => {
+    setLoading(true);
+    // deleting user
+    deleteUser(currentUser)
+      .then(() => {
+        alert("your account has been deleted");
+      })
+      .catch((error) => {
+        alert("some error occured while deleting account ");
+        console.log("some error occured while deleting account ", error);
       });
     setLoading(false);
   };
@@ -122,11 +250,13 @@ const UserAuthProvider = ({ children }) => {
     currentUser,
     UserLogin,
     UserLogout,
-    ProfileInformation,
     verifyEmail,
     UploadAvatar,
     UpdateName,
     UpdateEmail,
+    UpdatePassword,
+    DeleteUser,
+    storeUserData,
   };
   return <userContext.Provider value={value}>{children}</userContext.Provider>;
 };

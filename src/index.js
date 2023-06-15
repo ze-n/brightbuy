97ed7380813b802c85fbf2e4ea14076407a5b0b9
shoyo <senku_snap@protonmail.com>
2023-06-15 17:21:55 +0530

    added ProductsProvider in index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import UserAuthProvider from "./context/UserAuthContext";
-
+import ProductsProvider from "./context/ProductsContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserAuthProvider>
-      <App />
+      <ProductsProvider>
+        <App />
+      </ProductsProvider>
     </UserAuthProvider>
   </React.StrictMode>
 );

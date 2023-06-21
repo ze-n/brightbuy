dd95971a463d026c5414751de211b773b7eac702
shoyo <senku_snap@protonmail.com>
2023-06-21 05:38:31 +0530

    got data from storgage and passed it to productcard

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import API from "./Temp";
 import ProductCard from "./ProductCard";
+import { useProducts } from "../../context/ProductsContext";
+
 const Products = () => {
+  const [cardComponents, setCardComponents] = useState(null);
+  const { getProductCollection } = useProducts();
+  useEffect(() => {
+    const productType = "CLOTHING";
+    getProductCollection(productType)
+      .then((products) => {
+        const components = products.map((product) => {
+          const productId = product.id; // Get the ID of the product
+          const productData = product; // Get the data of the product
+
+          // Access specific fields of the product document
+          const {
+            productName,
+            productReviews,
+            productRating,
+            productPrice,
+            productImages,
+          } = productData;
+          const coverImg = productImages[0];
+
+          // Return the card component with product details as props
+          return (
+            <ProductCard
+              id={productId}
+              name={productName}
+              price={productPrice}
+              reviews={productReviews}
+              rating={productRating}
+              image={coverImg}
+            />
+          );
+        });
+
+        setCardComponents(components);
+      })
+      .catch((error) => {
+        console.log("Error retrieving product collection:", error.message);
+        return null;
+      });
+  }, []);
+
   return (
     <Wrapper>
       <div className="container products">
-        {API.map((e) => {
-          const { id, name, reviews, rating, price, images } = e;
-
-          return (
-            <ProductCard
-              id={id}
-              name={name}
-              price={price}
-              reviews={reviews}
-              rating={rating}
-              image={images[0]}
-            />
-          );
-        })}
+        {" "}
+        {cardComponents ? cardComponents : "No products available."}
       </div>
     </Wrapper>
   );
